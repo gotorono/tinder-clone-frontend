bc88343ccbb333108dab202bfddce373bd54165c
gotorono <gotercze@seznam.cz>
2020-11-18 00:00:30 +0100

    handleclick not done yet

--- src/SwipeButtons.js
+++ src/SwipeButtons.js
@@ -9,22 +9,27 @@ import FlashOnIcon from "@material-ui/icons/FlashOn"
 
 import IconButton from "@material-ui/core/IconButton"
 
+
+const handleClick = value => {
+    console.log(value);
+}
+
 function SwipeButtons() {
     return (
         <div className="swipeButtons">
-            <IconButton className="swipeButtons__ replay">
+            <IconButton onMouseUp={handleClick('replay')} className="swipeButtons__ replay">
                 <ReplayIcon className="swipeButton" />
             </IconButton>
-            <IconButton className="swipeButtons__ left">
+            <IconButton onMouseUp={handleClick('left')} className="swipeButtons__ left">
                 <CloseIcon className="swipeButton" />
             </IconButton>
-            <IconButton className="swipeButtons__ star">
+            <IconButton onMouseUp={handleClick('star')} className="swipeButtons__ star">
                 <StarRateIcon className="swipeButton" />
             </IconButton>
-            <IconButton className="swipeButtons__ right">
+            <IconButton onMouseUp={handleClick('right')} className="swipeButtons__ right">
                 <FavoriteIcon className="swipeButton" />
             </IconButton>
-            <IconButton className="swipeButtons__ lightning">
+            <IconButton onMouseUp={handleClick('lightning')} className="swipeButtons__ lightning">
                 <FlashOnIcon className="swipeButton" />
             </IconButton>
         </div>
--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,97 +1,91 @@
-import React, { useState, useEffect } from 'react'
-import TinderCard from 'react-tinder-card';
-import './TinderCards.css'
-import { updateUser, getCards } from './actions/authActions';
-import axios from './axios';
+import React, { useState, useEffect } from "react";
+import TinderCard from "react-tinder-card";
+import "./TinderCards.css";
+import { updateUser, getCards } from "./actions/authActions";
+import axios from "./axios";
 
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 
 function TinderCards(props) {
-    const [people, setPeople] = useState([]);
+  const [people, setPeople] = useState([]);
 
-    useEffect(() => {
-        console.log(props.auth.user.cards);
-        console.log(props.auth.user.swiped)
+  useEffect(() => {
+    console.log(props.auth.user.cards);
+    console.log(props.auth.user.swiped);
 
-        async function updateUser() {
-            const userData = {
-                user: props.auth.user
-            }
+    updateUser();
+  }, []);
 
-            await props.updateUser(userData);
-            fetchData();
-        }
+  async function fetchData() {
+    const req = await axios.get("/tinder/cards", {
+      params: { user: props.auth.user.id },
+    });
+    setPeople(req.data);
+  }
 
-        updateUser();
-
-        async function fetchData() {
-            const req = await axios.get('/tinder/cards', {params: {user: props.auth.user.id}});
-            setPeople(req.data);
-        }
-        
-
-    }, [])
-
-    const updateSwiped = () => {
-        const userData = {
-            user: props.auth.user
-        }
-        async function update() {
-            await props.updateUser(userData);
-        }
-        update();
-    }
-
-    const swiped = (direction, user) => {
-
-        async function push() {
-        let error = false;
-        await axios.post('/tinder/push', {userId: props.auth.user.id, swipedId: user._id})
-        .catch(error => {
-            error = true;
-        })
-
-        if(!error) {
-            updateSwiped();
-        }
-    }
-    push();
+  async function updateUser() {
+    const userData = {
+      user: props.auth.user,
     };
 
-    const outOfFrame = (name) => {
-        console.log(name + " has left the screen");
+    await props.updateUser(userData);
+    fetchData();
+  }
+
+  const swiped = (direction, user) => {
+      let a = people;
+      a.splice(a.findIndex(e => e._id === user._id), 1);
+      setPeople(a);
+    async function push() {
+      await axios
+        .post("/tinder/push", {
+          userId: props.auth.user.id,
+          swipedId: user._id,
+        })
+        .then(people.length < 2 ? updateUser() : null);
     }
+    push();
+  };
 
+  const outOfFrame = (name) => {
+    console.log(name + " has left the screen");
+  };
 
-    return (
-        <div className="tinderCards">
-            <div className="tinderCards__cardContainer">
-            { 
-            people.map((person) => (
-                <TinderCard
-                className="swipe"
-                key={person._id}
-                preventSwipe={['up', 'down']}
-                onSwipe={(dir) => swiped(dir, person)}
-                onCardLeftScreen={() => outOfFrame(person.name)}
-                >
-                    <div style={ {backgroundImage: `url(${person.profileImg})`} }
-                    className="card">
-                        <div className="desc-container">
-            <h3>{person.name}<span className="age"> &nbsp;{person.age}</span></h3>
-                        <div className="desc">{person.desc}</div>
-                        </div>
-                    </div>
-                </TinderCard>
-            ))}
+  return (
+    <div className="tinderCards">
+      <div className="tinderCards__cardContainer">
+        {people.map((person) => (
+          <TinderCard
+            className="swipe"
+            key={person._id}
+            preventSwipe={["up", "down"]}
+            onSwipe={(dir) => swiped(dir, person)}
+            onCardLeftScreen={() => outOfFrame(person.name)}
+          >
+            <div
+              style={{ backgroundImage: `url(${person.profileImg})` }}
+              className="card"
+            >
+              <div className="desc-container">
+                <h3>
+                  {person.name}
+                  <span className="age"> &nbsp;{person.age}</span>
+                </h3>
+                <div className="desc">{person.desc}</div>
+              </div>
             </div>
-        </div>
-    )
+          </TinderCard>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 const mapStateToProps = (state) => ({
-    auth: state.auth
-  });
+  auth: state.auth,
+});
 
-export default connect(mapStateToProps, { updateUser })(withRouter(TinderCards));
+export default connect(mapStateToProps, { updateUser })(
+  withRouter(TinderCards)
+);
