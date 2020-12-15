1a41a6f58f7e0222d24a09c48b49dc082862167e
gotorono <gotercze@seznam.cz>
2020-12-15 08:47:05 +0100

    back to localhost

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://tinder-clone-goter.herokuapp.com/'//'http://localhost:8001'
+    baseURL: 'http://localhost:8001'//'https://tinder-clone-goter.herokuapp.com/'
 });
 
 export default instance;
